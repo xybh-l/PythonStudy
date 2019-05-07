f77abe75a1cd7fcbfd33f8e826743c6cc2d00e71
a1353433900 <42896164+a1353433900@users.noreply.github.com>
2019-05-07 15:14:30 +0800

    requests库高级应用

--- Python爬虫开发实战/requests库高级用法/SSL证书验证.py
+++
@@ -1,4 +0,0 @@
-import requests
-
-response = requests.get('https://study.lanwang.online', verify=False)
-print(response.status_code)
--- Python爬虫开发实战/requests库高级用法/文件上传.py
+++
@@ -1,5 +0,0 @@
-import requests
-
-files = {'file': open('../favicon.ico', 'rb')}
-r = requests.post("http://httpbin.org/post", files=files)
-print(r.text)
